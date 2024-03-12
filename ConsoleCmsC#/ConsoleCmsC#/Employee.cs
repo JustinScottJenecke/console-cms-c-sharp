4b800f41abff02afcd0d255b8e3410b87263079e
codespace-justin <justin@codespace.co.za>
2024-03-12 09:54:41 +0200

    creates employee class and tests out table print formatting

--- ConsoleCmsC#/ConsoleCmsC#/Employee.cs
+++ ConsoleCmsC#/ConsoleCmsC#/Employee.cs
@@ -4,9 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleCmsC_
+namespace ConsoleCmsCS
 {
     class Employee
     {
+        private int EmployeeId { get; set;}
+        private string Name { get; set; }
+        private string Role { get; set; }
+
+        public Employee(int eID, string name, string role)
+        {
+            this.EmployeeId = eID;
+            this.Name = name;
+            this.Role = role;
+        }
+
+        public static string employeeDisplayHeaderRow()
+        {
+            return " ID): \t|\t Employee Name: \t|\t Role:";
+        }
+        public string employeeToString() {
+            return $" {this.EmployeeId}) \t|\t {this.Name} \t|\t {this.Role}";
+        }
     }
 }
