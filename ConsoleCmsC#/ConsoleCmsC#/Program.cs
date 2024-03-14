07ab40f6290bf78512a10d1e7eb581945f529993
codespace-justin <justin@codespace.co.za>
2024-03-14 08:12:35 +0200

    fixes terminal bug where result of TryParse wont show error message

--- ConsoleCmsC#/ConsoleCmsC#/Program.cs
+++ ConsoleCmsC#/ConsoleCmsC#/Program.cs
@@ -19,9 +19,20 @@ namespace ConsoleCmsCS
                     case 0:
                         menu.StartupMenu();
                         Console.WriteLine("Choice: ");
-                        int input = Int32.Parse(Console.ReadLine());
-                        STATE = input;
-                        Console.Clear();
+                        int input;
+
+                        if (Int32.TryParse(Console.ReadLine(), out input))
+                        {
+                            STATE = input;
+                        }
+                        else 
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Invalid input. Please insert a number as your to select an option from the menu..");
+                            Console.ReadKey();
+                            STATE = input;
+                            Console.Clear();
+                        }
                         break;
                     case 1:
                         menu.AddEmployee(menu.Employees);
